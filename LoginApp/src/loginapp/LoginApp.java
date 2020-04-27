843ed7e618424543dbc08256f509fd5f2a7fc3af
David Boh <herrboh@live.com>
2020-04-27 15:33:34 -0500

    april 27th

---
+++ LoginApp/src/loginapp/LoginApp.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package loginapp;
+
+/**
+ *
+ * @author herrboh
+ */
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class LoginApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();// TODO code application logic here
+    }
+    
+}
